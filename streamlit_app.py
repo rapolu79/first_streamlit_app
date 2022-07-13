f9a9632a283c361b730b3be5d5d6896d85766d6f
Jagan Rapolu <63914341+rapolu79@users.noreply.github.com>
2022-07-13 17:45:27 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Blueberry Oatmeal')
+streamlit.text('Kale, Spinach Smoothie')
+streamlit.text('Boiled Eggs')
