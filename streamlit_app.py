5e99111749fe27b87f71d0b31026927e19627bdc
Jagan Rapolu <63914341+rapolu79@users.noreply.github.com>
2022-07-19 08:40:40 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,18 +27,15 @@ try:
     streamlit.error("Please select a fruit for information.")
   else:
 #streamlit.write('The user entered ', fruit_choice)
-
 #import requests
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
-
 # write your own comment -what does the next line do? 
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-  streamlit.dataframe(fruityvice_normalized)
+    streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
   streamlit.error()
-
 streamlit.stop()
 
 #import snowflake.connector
